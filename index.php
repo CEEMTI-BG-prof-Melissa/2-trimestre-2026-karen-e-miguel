c23814df780b0b42e3434cfba3f4ea2d63e6ea8e
karencbgabriel <karen.cbgabriel@aluno.edu.es.gov.br>
2026-07-01 07:49:33 -0300

    index.php aula 02

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+     <title>Aula PHP</title>
+</head>
+<body>
+    <?php
+    $nomem = "Miguel";
+    $idadem = 16;
+    $nomek = "Karen";
+    $idadek = 16;
+
+    echo "Meu nome é $nomem, tenho $idadem anos.<br>";  "O nome dela é $nomek, e ela tem $idadek anos.";
+?>
+
+</body>
+</html>
+
+
